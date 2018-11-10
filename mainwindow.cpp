d5ba8247eec656cbb1ee055f9cf6ef8d5de73c3b
Haden <admin@sunhx.cn>
2018-11-10 14:52:05 +0800

    Initial commit

---
+++ mainwindow.cpp
@@ -0,0 +1,272 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+//#pragma execution_character_set("utf-8")
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    setWindowTitle(QString::fromUtf8("仓储调度"));                  //修改标题
+    setAutoFillBackground(true);                //修改背景颜色，必须有这条语句
+    setPalette(QPalette(QColor(255,204,1)));
+    QPixmap pixmap("DHL.jpg");                  //加载logo
+    QPixmap fitpixmap = pixmap.scaled(480, 200, Qt::KeepAspectRatio, Qt::SmoothTransformation);
+    ui->label->setPixmap(fitpixmap);
+    showMaximized();
+    id_number = _ID_NUMBER;
+
+    freshTable();
+
+    socket = new QTcpSocket(this);
+    isReceved = 1;
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_connectButton_clicked()
+{
+    if(!socket->isOpen())
+    {
+        //TCP连接
+        int port = _PORT;
+        socket = new QTcpSocket(this);
+        socket->connectToHost(_IP,port);
+
+        //连接槽
+        connect(socket,SIGNAL(readyRead()),this,SLOT(socket_readPendingDatagrams()));
+        connect(socket,SIGNAL(disconnected()),this,SLOT(socket_disconnect()));
+
+        //ui更新
+        ui->connectButton->setText(QString::fromUtf8("断开"));
+
+    }
+    else
+    {
+        //TCP关闭
+        socket->close();
+        disconnect(socket,SIGNAL(readyRead()),this,SLOT(socket_readPendingDatagrams()));
+
+        //UI更新
+        ui->connectButton->setText(QString::fromUtf8("连接"));
+    }
+}
+
+void MainWindow::socket_readPendingDatagrams()
+{
+    //接受数据
+    isReceved = 1;
+}
+
+int MainWindow::packetCreate(int APICode, int packetId, char* jsonData,char *data)
+{
+    int len = 0;
+    ProtocolHeader *protolHeader = (ProtocolHeader*)data;
+    protolHeader->m_sync = 90;
+    protolHeader->m_version = 1;
+    protolHeader->m_number = (uint16_t)packetId << 8 | (packetId>>8 & 0x0F);
+    protolHeader->m_type = (uint16_t)APICode << 8 | (APICode>>8 & 0x0F);
+    protolHeader->m_reserved[0] = 0;
+    protolHeader->m_reserved[1] = 0;
+    protolHeader->m_reserved[2] = 0;
+    protolHeader->m_reserved[3] = 0;
+    protolHeader->m_reserved[4] = 0;
+    protolHeader->m_reserved[5] = 0;
+    len = strlen(jsonData);
+    uint32_t l = (uint32_t)len;
+    protolHeader->m_length =  ((l & 0x00FF) << 24) | ((l & 0xff00) << 8);
+    memcpy(data+16,jsonData,strlen(jsonData));
+    data[16+len] = 0;
+
+    return strlen(jsonData) + 16;
+}
+
+
+void MainWindow::on_deleButton_clicked()
+{
+    QPushButton *btn = (QPushButton*)sender();
+    QModelIndex index = ui->tableWidget->indexAt(btn->pos());
+    ui->tableWidget->removeRow(index.row());
+    //QMessageBox::information(NULL,"title",QString::number(index.row()),QMessageBox::Yes);
+
+}
+
+void MainWindow::on_takeButton_clicked()
+{
+
+
+    if(isReceved && socket->isOpen())
+    {
+        QPushButton *btn = (QPushButton*)sender();
+        QString position = btn->property("position").toString();
+        int row = ui->tableWidget->indexAt(btn->pos()).row();
+        ui->tableWidget->item(row,2)->setText(QString::fromUtf8("取货"));
+
+
+        char data[100];
+        char json[100];
+
+        if(position == "A")
+        {
+            char json_data[] = "{\"name\":\"tasks1_2\"}";
+            memcpy(json,json_data,strlen(json_data));
+            json[strlen(json_data)] = 0;
+        }
+        else if(position == "B")
+        {
+            char json_data[] = "{\"name\":\"tasks1_3\"}";
+            memcpy(json,json_data,strlen(json_data));
+            json[strlen(json_data)] = 0;
+        }
+        else if(position == "C")
+        {
+            char json_data[] = "{\"name\":\"tasks1_4\"}";
+            memcpy(json,json_data,strlen(json_data));
+            json[strlen(json_data)] = 0;
+        }
+        else if(position == "D")
+        {
+            char json_data[] = "{\"name\":\"tasks1_5\"}";
+            memcpy(json,json_data,strlen(json_data));
+            json[strlen(json_data)] = 0;
+        }
+        int len = packetCreate(3106,12,json,data);
+        socket->write(data,len);
+
+        isReceved = 0;
+    }
+    else
+    {
+        QMessageBox::warning(NULL,QString::fromUtf8("警告"),QString::fromUtf8("等待上一条指令执行！"),QMessageBox::Yes);
+    }
+
+    //QMessageBox::information(NULL,"title",position,QMessageBox::Yes);
+}
+
+void MainWindow::on_deliverButton_clicked()
+{
+
+
+    if(isReceved && socket->isOpen())
+    {
+        QPushButton *btn = (QPushButton*)sender();
+        QString position = btn->property("position").toString();
+        int row = ui->tableWidget->indexAt(btn->pos()).row();
+        ui->tableWidget->item(row,2)->setText(QString::fromUtf8("送货"));
+
+
+        char data[100];
+        char json[100];
+
+        if(position == "A")
+        {
+            char json_data[] = "{\"name\":\"tasks2_1\"}";
+            memcpy(json,json_data,strlen(json_data));
+            json[strlen(json_data)] = 0;
+        }
+        else if(position == "B")
+        {
+            char json_data[] = "{\"name\":\"tasks3_1\"}";
+            memcpy(json,json_data,strlen(json_data));
+            json[strlen(json_data)] = 0;
+        }
+        else if(position == "C")
+        {
+            char json_data[] = "{\"name\":\"tasks4_1\"}";
+            memcpy(json,json_data,strlen(json_data));
+            json[strlen(json_data)] = 0;
+        }
+        else if(position == "D")
+        {
+            char json_data[] = "{\"name\":\"tasks5_1\"}";
+            memcpy(json,json_data,strlen(json_data));
+            json[strlen(json_data)] = 0;
+        }
+        int len = packetCreate(3106,12,json,data);
+        socket->write(data,len);
+
+        isReceved = 0;
+    }
+    else
+    {
+        QMessageBox::warning(NULL,QString::fromUtf8("警告"),QString::fromUtf8("等待上一条指令执行！"),QMessageBox::Yes);
+    }
+
+    //QMessageBox::information(NULL,"title",position,QMessageBox::Yes);
+}
+
+void MainWindow::freshTable()
+{
+    //表格部分
+    QStringList header;
+    header<<QString::fromUtf8(" 订单编号 ")<<" 库位 "<<"模式"<<"  取货  "<<"  送货  "<<"  控制  ";
+
+    int count = ui->tableWidget->rowCount();
+    if(count == 0)
+    {
+        ui->tableWidget->setColumnCount(6);
+        ui->tableWidget->setColumnWidth(0,1100);
+        ui->tableWidget->setColumnWidth(1,400);
+        ui->tableWidget->setColumnWidth(2,400);
+        ui->tableWidget->setEditTriggers(QAbstractItemView::NoEditTriggers);    //禁止编辑
+        ui->tableWidget->setSelectionBehavior(QAbstractItemView::SelectItems);
+        ui->tableWidget->setSelectionMode(QAbstractItemView::SingleSelection);
+        ui->tableWidget->setHorizontalHeaderLabels(header);
+        ui->tableWidget->horizontalHeader()->setStretchLastSection(true);
+    }
+
+    if(count < 40)
+    {
+        qsrand(QTime(0,0,0).secsTo(QTime::currentTime()));
+        for(int i = count; i < 40;i++)
+        {
+            ui->tableWidget->insertRow(i);              //插入行
+            ui->tableWidget->setRowHeight(i,100);       //设置行高
+
+            QDateTime data = QDateTime::currentDateTime();
+            QString data_string = data.toString("yyyyMMdd");
+            data_string += QString("%1").arg(id_number,4,10,QLatin1Char('0'));
+            id_number++;
+            ui->tableWidget->setItem(i,0,new QTableWidgetItem(data_string));
+
+            QChar position = 'A' + qrand()%_POINT_NUMBER;
+            ui->tableWidget->setItem(i,1,new QTableWidgetItem(position));
+
+            ui->tableWidget->setItem(i,2,new QTableWidgetItem("等待"));
+
+            QPushButton *takeButton = new QPushButton();
+            takeButton->setText("取货");
+            takeButton->setProperty("position",position);
+            takeButton->setProperty("row",i);
+            connect(takeButton,SIGNAL(clicked()),this,SLOT(on_takeButton_clicked()));
+            ui->tableWidget->setCellWidget(i,3,takeButton);
+
+            QPushButton *deliverButton = new QPushButton();
+            deliverButton->setText("送货");
+            deliverButton->setProperty("position","B");
+            connect(deliverButton,SIGNAL(clicked()),this,SLOT(on_deliverButton_clicked()));
+            ui->tableWidget->setCellWidget(i,4,deliverButton);
+
+            QPushButton *deleButton = new QPushButton();
+            deleButton->setText("删除");
+            connect(deleButton,SIGNAL(clicked()),this,SLOT(on_deleButton_clicked()));
+            ui->tableWidget->setCellWidget(i,5,deleButton);
+
+        }
+
+    }
+}
+
+void MainWindow::on_refreshButton_clicked()
+{
+    freshTable();
+}
+
+void MainWindow::socket_disconnect()
+{
+    ui->connectButton->setText("连接");
+}
---
+++ mainwindow.h
@@ -0,0 +1,64 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <QMainWindow>
+#include <QtNetwork/QtNetwork>
+#include <QMessageBox>
+#include <QDateTime>
+
+
+
+#define _IP "127.0.0.1"
+#define _PORT 19206
+#define _POINT_NUMBER 4
+#define _ID_NUMBER 2034
+
+
+
+struct ProtocolHeader {
+    uint8_t  m_sync;
+    uint8_t  m_version;
+    uint16_t m_number;
+    uint32_t m_length;
+    uint16_t m_type;
+    uint8_t  m_reserved[6];
+};
+
+
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private slots:
+    void on_connectButton_clicked();
+    void socket_readPendingDatagrams();
+
+
+    void on_deleButton_clicked();
+    void on_takeButton_clicked();
+    void on_deliverButton_clicked();
+    void on_refreshButton_clicked();
+    void socket_disconnect();
+
+private:
+    Ui::MainWindow *ui;
+    QTcpSocket *socket;
+    int isReceved;          //是否收到回复
+    int id_number;          //id序号
+
+    int packetCreate(int APICode, int packetId, char* jsonData,char *data);
+    void freshTable();
+};
+
+#endif // MAINWINDOW_H
